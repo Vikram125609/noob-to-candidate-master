334654aee86a38ede27a56cc445bc4fd0bb4386d
Vikram Singh <vs125609@gmail.com>
2022-12-16 16:03:29 +0530

    modified sieve of eratothenes

--- Coding Mafia/Codechef/Sieve-of-eratosthenes.cpp
+++ Coding Mafia/Codechef/Sieve-of-eratosthenes.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
+#include<math.h>
 #include<vector>
 using namespace std;
 void soe(int n) {
     vector<int> prime(n,0);
-    for(int i = 2;i < n; i++) {
+    for(int i = 2;i < sqrt(n); i++) {
         if(prime[i] == 0) {
-            for(int j = i * i;j <= n; j+= i) {
+            for(int j = i * i;j < n; j+= i) {
                 prime[j] = 1;
             }
         }
