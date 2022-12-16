47651e338053a5fef6dd07d19d261e3f4b582fd1
Vikram Singh <vs125609@gmail.com>
2022-12-16 10:46:45 +0530

    Folder name changed

--- Coding Mafia/Basics/codingmafia.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout << "This program was generate by Vikram Singh" << endl;
-    // DJSCE 2024
-    return 0;
-}
--- Coding Mafia/Basics/string.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    string s("a", 5);
-    cout << s << endl;
-    return 0;
-}
