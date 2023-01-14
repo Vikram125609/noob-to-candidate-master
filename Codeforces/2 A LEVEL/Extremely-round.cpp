614cbe18930bf00a2f63440651725a45dd47dc3f
Vikram Singh <vs125609@gmail.com>
2023-01-14 15:59:20 +0530

    800 Level Problem Simple

--- Codeforces/2 A LEVEL/Extremely-round.cpp
+++ Codeforces/2 A LEVEL/Extremely-round.cpp
@@ -1,27 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <math.h>
 using namespace std;
-void solve() {
+void solve()
+{
     int n;
     cin >> n;
-    int count = 0;
-    for(int i = 1;i <= n; i++) {
-        int k = i;
-        int count1 = 0;
-        while(k) {
-            int last_digit = k % 10;
-            if(last_digit != 0) {
-                count1++;
-            }
-            k = k / 10;
-        }
-        if(count1 == 1) count++;
+    if(n <= 9) {
+        cout << n << endl;
+        return;
     }
-    cout << count << endl;
+    int count = 0;
+    int num;
+    while (n > 0)
+    {
+        count++;
+        num = n;
+        n = n / 10;
+    }
+    cout << 10 + (count - 2) * 9 + num - 1 << endl;
 }
 int main()
 {
     int t;
     cin >> t;
-    while(t--) solve();
+    while (t--)
+        solve();
     return 0;
 }
